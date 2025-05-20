ea3e191f1d7797384446aa587c62aab966e1e11d
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-05-20 20:55:26 +0530

    gameinfo

--- dino.c
+++ dino.c
@@ -15,3 +15,25 @@ void moveTo(int x, int y)
     SetConsoleCursorPosition(
         GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
+// Function to pause execution for a given number of
+// milliseconds
+void pause(unsigned int milliseconds)
+{
+    clock_t goal = milliseconds + clock();
+    while (goal > clock())
+        ;
+}
+
+// Function to display game information on the console
+void displayGameInfo()
+{
+    system("cls");
+    moveTo(10, 2);
+    printf("Press X to Exit, Press Space to Jump");
+    moveTo(62, 2);
+    printf("SCORE : ");
+    moveTo(1, 25);
+    for (int x = 0; x < 79; x++)
+        printf("п");
+}
