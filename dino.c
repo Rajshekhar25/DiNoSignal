2fcdd8bbd4b9ac007d89edaf03d9d24c495a5930
RAJSHEKHAR DAS <rajshekhardas85@gmail.com>
2025-05-21 23:04:25 +0530

    characters

--- dino.c
+++ dino.c
@@ -37,3 +37,69 @@ void displayGameInfo()
     for (int x = 0; x < 79; x++)
         printf("п");
 }
+
+// Global variables for jump height and game speed
+int jumpHeight, gameSpeed = 40;
+
+// Function to display the character on the console
+void displayCharacter(int jumpType = 0)
+{
+    static int animationState = 1;
+
+    // Update the jump height based on the jump type
+    if (jumpType == 0)
+        jumpHeight = 0;
+    else if (jumpType == 2)
+        jumpHeight--;
+    else
+        jumpHeight++;
+
+    // Display the character at the specified position
+    moveTo(2, 15 - jumpHeight);
+    printf("                ");
+    moveTo(2, 16 - jumpHeight);
+    printf("     мллл      ");
+    moveTo(2, 17 - jumpHeight);
+    printf("    ллллллл     ");
+    moveTo(2, 18 - jumpHeight);
+    printf("   ллллллллл    ");
+    moveTo(2, 19 - jumpHeight);
+    printf("  лллллплллл   ");
+    moveTo(2, 20 - jumpHeight);
+    printf(" млллллппллллм  ");
+    moveTo(2, 21 - jumpHeight);
+    printf(" ллллллпплллллл ");
+    moveTo(2, 22 - jumpHeight);
+    printf(" пллллллппллллп ");
+    moveTo(2, 23 - jumpHeight);
+    if (jumpType == 1 || jumpType == 2) {
+        printf("   ллп плпппл   ");
+        moveTo(2, 24 - jumpHeight);
+        printf("   лм   лм      ");
+    }
+    else if (animationState == 1) {
+        printf("  пллп  ппп     ");
+        moveTo(2, 24 - jumpHeight);
+        printf("     лм         ");
+        animationState = 2;
+    }
+    else if (animationState == 2) {
+        printf("   плм пл       ");
+        moveTo(2, 24 - jumpHeight);
+        printf("         лм     ");
+        animationState = 1;
+    }
+
+    moveTo(2, 25 - jumpHeight);
+    if (jumpType != 0) {
+        printf("                ");
+    }
+    else {
+        printf("ппппппппппппппппп");
+    }
+    pause(gameSpeed);
+}
+
+
+
+    
